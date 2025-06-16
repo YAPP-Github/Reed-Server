48afb615f9261138a5996a4e4163791500d16c6d
DongHoon Lee <dhl1924@naver.com>
2025-06-16 21:33:10 +0900

    chore: 도메인 모듈에 jpa 의존성 등록

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,6 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 dependencies {
     implementation(project(Dependencies.Projects.GLOBAL_UTILS))
     implementation(Dependencies.Spring.BOOT_STARTER_WEB)
+    implementation(Dependencies.Spring.BOOT_STARTER_DATA_JPA)
     testImplementation(Dependencies.Spring.BOOT_STARTER_TEST)
 }
 
