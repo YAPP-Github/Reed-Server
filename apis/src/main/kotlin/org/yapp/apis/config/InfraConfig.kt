7f762b9eef1e6a3876c0e708c9341d1cbd411a7c
DongHoon Lee <dhl1924@naver.com>
2025-06-16 19:53:27 +0900

    feat: Apis 모듈에 모듈별 인프라 설정 자동 등록 적용
    - Apis 모듈 InfraConfig에 @EnableInfraBaseConfig 어노테이션 적용
    - InfraBaseConfigGroup.JPA와 ASYNC 설정을 통해 필요한 인프라 빈을 선택적으로 등록

---
+++ apis/src/main/kotlin/org/yapp/apis/config/InfraConfig.kt
@@ -0,0 +1,10 @@
+package org.yapp.apis.config
+
+import org.springframework.context.annotation.Configuration
+import org.yapp.EnableInfraBaseConfig
+import org.yapp.InfraBaseConfigGroup
+
+@Configuration(proxyBeanMethods = false)
+@EnableInfraBaseConfig([InfraBaseConfigGroup.JPA, InfraBaseConfigGroup.ASYNC])
+class InfraConfig {
+}
