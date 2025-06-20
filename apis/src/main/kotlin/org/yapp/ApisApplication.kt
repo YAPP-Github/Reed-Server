2efadf18af5db698a3f960e9551dd5ab31e2762f
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-06-21 01:11:19 +0900

    [BOOK-52] feat: apis -  application.yml 구성 (#11)

--- apis/src/main/kotlin/org/yapp/ApisApplication.kt
+++ apis/src/main/kotlin/org/yapp/ApisApplication.kt
@@ -3,9 +3,15 @@ package org.yapp
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Main application class for the apis module.
+ */
 @SpringBootApplication
 class ApisApplication
 
+/**
+ * Main function to start the application.
+ */
 fun main(args: Array<String>) {
     runApplication<ApisApplication>(*args)
 }
