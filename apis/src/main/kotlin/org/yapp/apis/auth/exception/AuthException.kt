c44cae5e659d87f5b60b3355fc5ec9cbb627643a
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-06-22 15:10:53 +0900

    [BOOK-52] refactor: apis - AuthException 수정 (#11)

--- apis/src/main/kotlin/org/yapp/apis/auth/exception/AuthException.kt
+++ apis/src/main/kotlin/org/yapp/apis/auth/exception/AuthException.kt
@@ -2,25 +2,7 @@ package org.yapp.apis.auth.exception
 
 import CommonException
 
-
-/**
- * Custom exception for authentication-related errors.
- * This exception is thrown when there are issues with authentication processes.
- */
-class AuthException : CommonException {
-
-    /**
-     * Constructor with error code.
-     *
-     * @param errorCode The error code.
-     */
-    constructor(errorCode: AuthErrorCode) : super(errorCode)
-
-    /**
-     * Constructor with error code and custom message.
-     *
-     * @param errorCode The error code.
-     * @param message The custom message.
-     */
-    constructor(errorCode: AuthErrorCode, message: String?) : super(errorCode, message)
-}
+class AuthException(
+    errorCode: AuthErrorCode,
+    message: String? = null
+) : CommonException(errorCode, message)
