563e68bc17e600d01d67b88b4d5cfd80f6d4803d
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-06-22 15:08:24 +0900

    [BOOK-52] refactor: apis - dto validation 추가  (#11)

--- apis/src/main/kotlin/org/yapp/apis/auth/dto/request/TokenRefreshRequest.kt
+++ apis/src/main/kotlin/org/yapp/apis/auth/dto/request/TokenRefreshRequest.kt
@@ -2,4 +2,10 @@ package org.yapp.apis.auth.dto.request
 
 data class TokenRefreshRequest(
     val refreshToken: String
-)
+) {
+    init {
+        require(refreshToken.isNotBlank()) {
+            "Refresh token must not be blank"
+        }
+    }
+}
