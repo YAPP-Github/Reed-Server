6b02b4a175be9fc1f820e049f8e734888305eb49
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-06-22 15:03:48 +0900

    [BOOK-52] refactor: apis - sealed 클래스 타입 강제  (#11)

--- apis/src/main/kotlin/org/yapp/apis/auth/service/AuthCredentials.kt
+++ apis/src/main/kotlin/org/yapp/apis/auth/service/AuthCredentials.kt
@@ -2,19 +2,18 @@ package org.yapp.apis.auth.service
 
 import org.yapp.domain.auth.ProviderType
 
-
-interface AuthCredentials {
-    fun getProviderType(): ProviderType
+sealed class AuthCredentials {
+    abstract fun getProviderType(): ProviderType
 }
 
 data class KakaoAuthCredentials(
     val accessToken: String
-) : AuthCredentials {
+) : AuthCredentials() {
     override fun getProviderType(): ProviderType = ProviderType.KAKAO
 }
 
 data class AppleAuthCredentials(
     val idToken: String
-) : AuthCredentials {
+) : AuthCredentials() {
     override fun getProviderType(): ProviderType = ProviderType.APPLE
 }
