c1f5fe3f3a55a381a1bed6eb2d64f409d5dd862b
DongHoon Lee <dhl1924@naver.com>
2025-06-24 23:46:50 +0900

    refactor: infra - 불필요한 어노테이션 제거

--- infra/src/main/kotlin/org/yapp/jpa/repository/JpaUserRepository.kt
+++ infra/src/main/kotlin/org/yapp/jpa/repository/JpaUserRepository.kt
@@ -1,7 +1,6 @@
 package org.yapp.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import org.yapp.domain.auth.ProviderType
 import org.yapp.domain.user.UserEntity
 import java.util.*
@@ -9,7 +8,6 @@ import java.util.*
 /**
  * JPA repository for UserEntity.
  */
-@Repository
 interface JpaUserRepository : JpaRepository<UserEntity, UUID> {
 
     fun findByProviderTypeAndProviderId(providerType: ProviderType, providerId: String): UserEntity?
