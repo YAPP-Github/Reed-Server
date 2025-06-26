3b889639a8ab98cacc0ff5a304e79e5bb42b3a6c
DongHoon Lee <dhl1924@naver.com>
2025-06-26 23:56:16 +0900

    [BOOK-67] chore: 패키지 이동

--- infra/src/main/kotlin/org/yapp/infra/external/redis/repository/RedisTokenRepository.kt
+++ infra/src/main/kotlin/org/yapp/infra/external/redis/repository/RedisTokenRepository.kt
@@ -1,4 +1,4 @@
-package org.yapp.infra.redis.repository
+package org.yapp.infra.external.redis.repository
 
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
