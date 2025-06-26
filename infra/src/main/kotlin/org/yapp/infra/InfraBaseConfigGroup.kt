2731f1c03ec4ebd4cbc9d3cd5a71d80aabc6e33a
DongHoon Lee <dhl1924@naver.com>
2025-06-26 23:54:38 +0900

    [BOOK-67] feat: Feign 설정 클래스 selector group에 등록

--- infra/src/main/kotlin/org/yapp/infra/InfraBaseConfigGroup.kt
+++ infra/src/main/kotlin/org/yapp/infra/InfraBaseConfigGroup.kt
@@ -1,5 +1,6 @@
 package org.yapp.infra
 
+import org.yapp.infra.config.external.oauth.FeignConfig
 import org.yapp.infra.config.internal.async.AsyncConfig
 import org.yapp.infra.config.internal.jpa.JpaConfig
 import org.yapp.infra.config.external.redis.RedisConfig
@@ -9,5 +10,6 @@ enum class InfraBaseConfigGroup(
 ) {
     ASYNC(AsyncConfig::class.java),
     JPA(JpaConfig::class.java),
-    REDIS(RedisConfig::class.java)
+    REDIS(RedisConfig::class.java),
+    OAUTH(FeignConfig::class.java)
 }
