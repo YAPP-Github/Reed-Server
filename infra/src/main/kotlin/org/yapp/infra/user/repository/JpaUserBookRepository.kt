e36fcf94f3fcb67fe7f618720d617eab1a6ea834
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-07-07 23:53:08 +0900

    [BOOK-96] feat: infra - UserBookRepository 구현 (#29)

---
+++ infra/src/main/kotlin/org/yapp/infra/user/repository/JpaUserBookRepository.kt
@@ -0,0 +1,10 @@
+package org.yapp.infra.user.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.yapp.infra.user.entity.UserBookEntity
+import java.util.*
+
+interface JpaUserBookRepository : JpaRepository<UserBookEntity, UUID> {
+    fun findByUserIdAndBookIsbn(userId: UUID, bookIsbn: String): UserBookEntity?
+    fun findAllByUserId(userId: UUID): List<UserBookEntity>
+}
---
+++ infra/src/main/kotlin/org/yapp/infra/user/repository/impl/UserBookRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package org.yapp.infra.user.repository.impl
+
+import org.springframework.stereotype.Repository
+import org.yapp.domain.book.UserBookRepository
+import org.yapp.domain.userbook.UserBook
+import org.yapp.infra.user.entity.UserBookEntity
+import org.yapp.infra.user.repository.JpaUserBookRepository
+import java.util.*
+
+@Repository
+class UserBookRepositoryImpl(
+    private val jpaUserBookRepository: JpaUserBookRepository
+) : UserBookRepository {
+
+    override fun findByUserIdAndBookIsbn(userId: UUID, isbn: String): UserBook? {
+        return jpaUserBookRepository.findByUserIdAndBookIsbn(userId, isbn)?.toDomain()
+
+    }
+
+    override fun save(userBook: UserBook): UserBook {
+        return jpaUserBookRepository.save(UserBookEntity.fromDomain(userBook)).toDomain()
+    }
+
+    override fun findAllByUserId(userId: UUID): List<UserBook> {
+        return jpaUserBookRepository.findAllByUserId(userId).map { it.toDomain() }
+    }
+}
