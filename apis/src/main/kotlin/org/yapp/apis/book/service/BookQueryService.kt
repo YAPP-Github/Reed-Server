91d69cf3dc661a0ad3fca84e6f4b594087378ff9
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-07-12 20:55:11 +0900

    [BOOK-96] refactor: apis- 외부 API 인터페이스 추상화 (#29)

--- apis/src/main/kotlin/org/yapp/apis/book/service/BookQueryService.kt
+++ apis/src/main/kotlin/org/yapp/apis/book/service/BookQueryService.kt
@@ -1,22 +1,12 @@
 package org.yapp.apis.book.service
 
-import org.springframework.stereotype.Service
 import org.yapp.apis.book.dto.request.BookDetailRequest
 import org.yapp.apis.book.dto.request.BookSearchRequest
 import org.yapp.apis.book.dto.response.BookDetailResponse
 import org.yapp.apis.book.dto.response.BookSearchResponse
 
 
-@Service
-class BookQueryService(
-    private val aladinBookQueryService: AladinBookQueryService
-) {
-    fun searchBooks(request: BookSearchRequest): BookSearchResponse {
-        return aladinBookQueryService.searchBooks(request)
-    }
-
-    fun getBookDetail(validIsbn: String): BookDetailResponse {
-        val request = BookDetailRequest.of(validIsbn)
-        return aladinBookQueryService.lookupBook(request)
-    }
+interface BookQueryService {
+    fun searchBooks(request: BookSearchRequest): BookSearchResponse
+    fun getBookDetail(request: BookDetailRequest): BookDetailResponse
 }
