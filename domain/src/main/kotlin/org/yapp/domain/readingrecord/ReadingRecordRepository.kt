6584426ba7d618ffe05706980f73ae7b452c24ec
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-07-25 00:01:35 +0900

    [BOOK-163] feat: domain - 통합 interface 구현 (#52)

---
+++ domain/src/main/kotlin/org/yapp/domain/readingrecord/ReadingRecordRepository.kt
@@ -0,0 +1,33 @@
+package org.yapp.domain.readingrecord
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.util.UUID
+
+
+interface ReadingRecordRepository {
+
+    fun save(readingRecord: ReadingRecord): ReadingRecord
+
+
+    fun findById(id: UUID): ReadingRecord?
+
+
+    fun findAllByUserBookId(userBookId: UUID): List<ReadingRecord>
+
+
+    fun findAllByUserBookId(userBookId: UUID, pageable: Pageable): Page<ReadingRecord>
+
+
+    fun findAllByUserBookIdIn(userBookIds: List<UUID>): List<ReadingRecord>
+
+
+    fun countByUserBookId(userBookId: UUID): Long
+
+
+    fun findReadingRecordsByDynamicCondition(
+        userBookId: UUID,
+        sort: String?,
+        pageable: Pageable
+    ): Page<ReadingRecord>
+}
