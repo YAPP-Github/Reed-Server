bf03a67a2907a7c6b0cc21a063b6318bc00feb79
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-07-24 23:59:36 +0900

    [BOOK-163] feat: infra - Jparepository 구현 (#52)

---
+++ infra/src/main/kotlin/org/yapp/infra/readingrecord/repository/impl/ReadingRecordRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package org.yapp.infra.readingrecord.repository.impl
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+import org.yapp.domain.readingrecord.ReadingRecord
+import org.yapp.domain.readingrecord.ReadingRecordRepository
+import org.yapp.infra.readingrecord.entity.ReadingRecordEntity
+import org.yapp.infra.readingrecord.repository.JpaReadingRecordRepository
+import java.util.UUID
+
+@Repository
+class ReadingRecordRepositoryImpl(
+    private val jpaReadingRecordRepository: JpaReadingRecordRepository
+) : ReadingRecordRepository {
+
+    override fun save(readingRecord: ReadingRecord): ReadingRecord {
+        val savedEntity = jpaReadingRecordRepository.saveAndFlush(ReadingRecordEntity.fromDomain(readingRecord))
+        return savedEntity.toDomain()
+    }
+
+    override fun findById(id: UUID): ReadingRecord? {
+        return jpaReadingRecordRepository.findByIdOrNull(id)?.toDomain()
+    }
+
+    override fun findAllByUserBookId(userBookId: UUID): List<ReadingRecord> {
+        val entities = jpaReadingRecordRepository.findAllByUserBookId(userBookId)
+        return entities.map { it.toDomain() }
+    }
+
+    override fun findAllByUserBookId(userBookId: UUID, pageable: Pageable): Page<ReadingRecord> {
+        val page = jpaReadingRecordRepository.findAllByUserBookId(userBookId, pageable)
+        return page.map { it.toDomain() }
+    }
+
+    override fun findAllByUserBookIdIn(userBookIds: List<UUID>): List<ReadingRecord> {
+        val entities = jpaReadingRecordRepository.findAllByUserBookIdIn(userBookIds)
+        return entities.map { it.toDomain() }
+    }
+
+    override fun countByUserBookId(userBookId: UUID): Long {
+        return jpaReadingRecordRepository.countByUserBookId(userBookId)
+    }
+
+    override fun findReadingRecordsByDynamicCondition(
+        userBookId: UUID,
+        sort: String?,
+        pageable: Pageable
+    ): Page<ReadingRecord> {
+        val page = jpaReadingRecordRepository.findReadingRecordsByDynamicCondition(userBookId, sort, pageable)
+        return page.map { it.toDomain() }
+    }
+}
