9684fdec06a3383da01c74767c7b9cdbc9586a62
DongHoon Lee <dhl1924@naver.com>
2025-07-29 02:30:31 +0900

    [BOOK-179] feat: apis - AuthErrorCode에 잘못된 요청 및 내부 서버 오류 관련 에러 코드 추가

--- apis/src/main/kotlin/org/yapp/apis/auth/exception/AuthErrorCode.kt
+++ apis/src/main/kotlin/org/yapp/apis/auth/exception/AuthErrorCode.kt
@@ -15,6 +15,7 @@ enum class AuthErrorCode(
     /* 400 BAD_REQUEST  */
     UNSUPPORTED_PROVIDER_TYPE(HttpStatus.BAD_REQUEST, "AUTH_005", "Unsupported provider type."),
     INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "AUTH_006", "Invalid credentials."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "AUTH_007", "Invalid request."),
     EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "AUTH_007", "Email not found."),
     INVALID_ID_TOKEN_FORMAT(HttpStatus.BAD_REQUEST, "AUTH_008", "Invalid ID token format."),
     SUBJECT_NOT_FOUND(HttpStatus.BAD_REQUEST, "AUTH_009", "Subject not found in ID token."),
@@ -28,7 +29,13 @@ enum class AuthErrorCode(
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_004", "Invalid access token."),
 
     /* 409 CONFLICT */
-    EMAIL_ALREADY_IN_USE(HttpStatus.CONFLICT, "AUTH_001", "Email already in use with a different account.");
+    EMAIL_ALREADY_IN_USE(HttpStatus.CONFLICT, "AUTH_001", "Email already in use with a different account."),
+
+    /* 500 INTERNAL_SERVER_ERROR */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_999", "Internal server error."),
+    FAILED_TO_LOAD_PRIVATE_KEY(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_998", "Failed to load Apple private key."),
+    INVALID_PRIVATE_KEY_FORMAT(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_997", "Invalid private key format."),
+    FAILED_TO_COMMUNICATE_WITH_PROVIDER(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH_996", "Failed to communicate with external provider.");
 
 
     override fun getHttpStatus(): HttpStatus = httpStatus
