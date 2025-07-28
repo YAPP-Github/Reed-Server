c8d878d652990a1674e1a4265d9922540964287d
DongHoon Lee <dhl1924@naver.com>
2025-07-29 02:30:55 +0900

    [BOOK-179] feat: apis - Apple 로그인용 client-secret 생성기 클래스 추가

---
+++ apis/src/main/kotlin/org/yapp/apis/auth/helper/apple/AppleClientSecretGenerator.kt
@@ -0,0 +1,37 @@
+package org.yapp.apis.auth.helper.apple
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm
+import org.springframework.security.oauth2.jwt.JwsHeader
+import org.springframework.security.oauth2.jwt.JwtClaimsSet
+import org.springframework.security.oauth2.jwt.JwtEncoder
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters
+import org.yapp.apis.config.AppleOauthProperties
+import org.yapp.globalutils.annotation.Helper
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+@Helper
+class AppleClientSecretGenerator(
+    private val appleProperties: AppleOauthProperties,
+    @Qualifier("appleJwtEncoder")
+    private val jwtEncoder: JwtEncoder,
+) {
+    fun generate(): String {
+        val header = JwsHeader.with(SignatureAlgorithm.ES256)
+            .keyId(appleProperties.keyId)
+            .build()
+
+        val claims = JwtClaimsSet.builder()
+            .issuer(appleProperties.teamId)
+            .subject(appleProperties.clientId)
+            .audience(listOf(appleProperties.audience))
+            .issuedAt(Instant.now())
+            .expiresAt(Instant.now().plus(5, ChronoUnit.MINUTES))
+            .build()
+
+        val encoderParameters = JwtEncoderParameters.from(header, claims)
+
+        return jwtEncoder.encode(encoderParameters).tokenValue
+    }
+}
