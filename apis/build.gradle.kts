fb422de9d68a8e7cf56ae3f2d8f6e8d1e9a53107
DongHoon Lee <dhl1924@naver.com>
2025-07-29 01:36:14 +0900

    [BOOK-179] chore: buildSrc, apis - 애플 로그인 로직에 사용될 의존성 추가

--- apis/build.gradle.kts
+++ apis/build.gradle.kts
@@ -11,12 +11,17 @@ dependencies {
     implementation(Dependencies.Spring.BOOT_STARTER_SECURITY)
     implementation(Dependencies.Spring.BOOT_STARTER_VALIDATION)
     implementation(Dependencies.Spring.BOOT_STARTER_ACTUATOR)
+    implementation(Dependencies.Spring.BOOT_STARTER_OAUTH2_CLIENT)
 
     implementation(Dependencies.Database.MYSQL_CONNECTOR)
 
     implementation(Dependencies.Swagger.SPRINGDOC_OPENAPI_STARTER_WEBMVC_UI)
 
     implementation(Dependencies.Logging.KOTLIN_LOGGING)
+
+    implementation(Dependencies.BouncyCastle.BC_PROV)
+
+    annotationProcessor(Dependencies.Spring.CONFIGURATION_PROCESSOR)
 
     testImplementation(Dependencies.Spring.BOOT_STARTER_TEST)
     testImplementation(Dependencies.TestContainers.MYSQL)
