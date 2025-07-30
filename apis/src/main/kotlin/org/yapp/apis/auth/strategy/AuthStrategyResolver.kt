459fde7ae94eb675baaea03c27acb02f9c4bb722
DongHoon Lee <dhl1924@naver.com>
2025-07-30 17:52:30 +0900

    [BOOK-179] chore: apis - 역할에 맞는 네이밍으로 변경

--- apis/src/main/kotlin/org/yapp/apis/auth/strategy/AuthStrategyResolver.kt
+++ apis/src/main/kotlin/org/yapp/apis/auth/strategy/AuthStrategyResolver.kt
@@ -1,16 +1,13 @@
-package org.yapp.apis.auth.service
+package org.yapp.apis.auth.strategy
 
 import org.springframework.stereotype.Service
-import org.yapp.apis.auth.strategy.AuthCredentials
 import org.yapp.apis.auth.exception.AuthErrorCode
 import org.yapp.apis.auth.exception.AuthException
-import org.yapp.apis.auth.strategy.AuthStrategy
 
 @Service
-class SocialAuthService(
+class AuthStrategyResolver(
     private val strategies: List<AuthStrategy>
 ) {
-
     fun resolve(credentials: AuthCredentials): AuthStrategy {
         return strategies.find { it.getProviderType() == credentials.getProviderType() }
             ?: throw AuthException(
