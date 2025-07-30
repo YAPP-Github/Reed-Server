1396a2b3e9da2c4bfe99b9cacf10d00dc1918277
DongHoon Lee <dhl1924@naver.com>
2025-07-31 00:33:04 +0900

    [BOOK-179] feat: infra - Apple OAuth 클라이언트 시크릿 포함 요청 반환 DTO 추가

---
+++ infra/src/main/kotlin/org/yapp/infra/external/oauth/apple/response/AppleTokenResponse.kt
@@ -0,0 +1,13 @@
+package org.yapp.infra.external.oauth.apple.response
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class AppleTokenResponse(
+    val accessToken: String,
+    val expiresIn: Int,
+    val idToken: String,
+    val refreshToken: String,
+    val tokenType: String
+)
