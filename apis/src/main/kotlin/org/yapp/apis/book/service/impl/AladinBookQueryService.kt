4b275fca056b6dd8e1a9788a47f7faf998852c66
DongHoon Lee <dhl1924@naver.com>
2025-08-01 22:00:48 +0900

    [BOOK-207] refactor: apis - AladinBookQueryService에서 Companion 객체 사용 제거 및 import 정리

--- apis/src/main/kotlin/org/yapp/apis/book/service/impl/AladinBookQueryService.kt
+++ apis/src/main/kotlin/org/yapp/apis/book/service/impl/AladinBookQueryService.kt
@@ -3,13 +3,13 @@ package org.yapp.apis.book.service.impl
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import org.yapp.apis.book.constant.BookQueryServiceQualifier
 import org.yapp.apis.book.dto.request.BookDetailRequest
 import org.yapp.apis.book.dto.request.BookSearchRequest
 import org.yapp.apis.book.dto.response.BookDetailResponse
 import org.yapp.apis.book.dto.response.BookSearchResponse
-import org.yapp.apis.book.constant.BookQueryServiceQualifier
+import org.yapp.apis.book.helper.AladinApiHelper
 import org.yapp.apis.book.service.BookQueryService
-import org.yapp.infra.external.aladin.helper.AladinApiHelper
 import org.yapp.infra.external.aladin.response.AladinBookDetailResponse
 import org.yapp.infra.external.aladin.response.AladinSearchResponse
 
@@ -24,7 +24,7 @@ class AladinBookQueryService(
         log.info("Service - Converting BookSearchRequest to AladinBookSearchRequest and calling Aladin API for book search.")
         val aladinSearchRequest = request.toAladinRequest()
         val response: AladinSearchResponse = aladinApiHelper.searchBooks(aladinSearchRequest)
-        return BookSearchResponse.Companion.from(response)
+        return BookSearchResponse.from(response)
     }
 
 
@@ -32,6 +32,6 @@ class AladinBookQueryService(
         log.info("Service - Converting BookDetailRequest to AladinBookLookupRequest and calling Aladin API for book detail lookup.")
         val aladinLookupRequest = request.toAladinRequest()
         val aladinResponse: AladinBookDetailResponse = aladinApiHelper.lookupBook(aladinLookupRequest)
-        return BookDetailResponse.Companion.from(aladinResponse)
+        return BookDetailResponse.from(aladinResponse)
     }
 }
