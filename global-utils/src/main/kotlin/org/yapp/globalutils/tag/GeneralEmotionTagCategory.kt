fffcddde89a929e5144540e20fc4889a6321f42f
DongHoon Lee <dhl1924@naver.com>
2025-08-01 21:04:43 +0900

    [BOOK-204] feat: global-utils - 일반 감정 태그 카테고리 열거형 추가

---
+++ global-utils/src/main/kotlin/org/yapp/globalutils/tag/GeneralEmotionTagCategory.kt
@@ -0,0 +1,18 @@
+package org.yapp.globalutils.tag
+
+enum class GeneralEmotionTagCategory(
+    val displayName: String
+) {
+    WARMTH("따뜻함"),
+    JOY("즐거움"),
+    TENSION("긴장감"),
+    SADNESS("슬픔");
+
+    companion object {
+        private val BY_DISPLAY_NAME = entries.associateBy { it.displayName }
+
+        fun fromDisplayName(displayName: String): GeneralEmotionTagCategory? {
+            return BY_DISPLAY_NAME[displayName]
+        }
+    }
+}
