cf02829790de40a48e2374295bce6404c4dacd09
DongHoon Lee <dhl1924@naver.com>
2025-08-04 02:42:54 +0900

    [BOOK-207] fix: infra - 중복된 필드 제거

--- infra/src/main/kotlin/org/yapp/infra/external/aladin/request/AladinBookLookupRequest.kt
+++ infra/src/main/kotlin/org/yapp/infra/external/aladin/request/AladinBookLookupRequest.kt
@@ -3,14 +3,12 @@ package org.yapp.infra.external.aladin.request
 data class AladinBookLookupRequest private constructor(
     val itemId: String,
     val itemIdType: String = "ISBN13",
-    val output: String?,
     val cover: String?
 ) {
     fun toMap(): Map<String, Any> {
         val params = mutableMapOf<String, Any>()
         params["ItemId"] = itemId
         params["ItemIdType"] = itemIdType
-        output?.let { params["Output"] = it }
         cover?.let { params["Cover"] = it }
         return params
     }
@@ -19,7 +17,7 @@ data class AladinBookLookupRequest private constructor(
         fun from(
             itemId: String,
         ): AladinBookLookupRequest {
-            return AladinBookLookupRequest(itemId, "ISBN13", "JS", "Big")
+            return AladinBookLookupRequest(itemId, "ISBN13", "Big")
         }
     }
 }
