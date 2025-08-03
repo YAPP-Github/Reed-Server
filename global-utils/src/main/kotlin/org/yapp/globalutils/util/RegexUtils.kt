d8d05933578e085a60e2b5ed5e2b117e9fc5684b
DongHoon Lee <dhl1924@naver.com>
2025-08-03 23:42:47 +0900

    [BOOK-207] refactor: global-utils - RegexUtils.kt에서 이메일 및 프로필 이미지 URL 패턴 제거하고 ISBN13 패턴 추가

--- global-utils/src/main/kotlin/org/yapp/globalutils/util/RegexUtils.kt
+++ global-utils/src/main/kotlin/org/yapp/globalutils/util/RegexUtils.kt
@@ -1,18 +1,5 @@
 package org.yapp.globalutils.util
 
 object RegexUtils {
-
-    val EMAIL_PATTERN = Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
-
-    val PROFILE_IMAGE_URL_PATTERN = Regex("^https?://[a-zA-Z0-9.-]+(/.*)?$")
-
-    const val NOT_BLANK_AND_NOT_NULL_STRING_PATTERN = "^(?!null$|NULL$|\\s*$).+" // Removed the old ISBN pattern
-
-    fun isValidEmail(email: String): Boolean {
-        return email.matches(EMAIL_PATTERN)
-    }
-
-    fun isValidProfileImageUrl(url: String): Boolean {
-        return url.matches(PROFILE_IMAGE_URL_PATTERN)
-    }
+    const val ISBN13_PATTERN = "^(978|979)\\d{10}$"
 }
