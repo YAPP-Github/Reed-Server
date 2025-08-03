894c9d5d479b8f456ccda8ee3a0dcac9511c4996
DongHoon Lee <dhl1924@naver.com>
2025-08-04 00:24:35 +0900

    [BOOK-207] feat: infra - Aladin API 응답을 위한 AladinDetailResponse 및 AladinSearchResponse 데이터 클래스 추가

---
+++ infra/src/main/kotlin/org/yapp/infra/external/aladin/response/AladinDetailResponse.kt
@@ -0,0 +1,52 @@
+package org.yapp.infra.external.aladin.response
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.math.BigDecimal
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AladinBookDetailResponse internal constructor(
+    @JsonProperty("version") val version: String?,
+    @JsonProperty("title") val title: String?,
+    @JsonProperty("link") val link: String?,
+    @JsonProperty("pubDate") val pubDate: String?,
+    @JsonProperty("totalResults") val totalResults: Int?,
+    @JsonProperty("startIndex") val startIndex: Int?,
+    @JsonProperty("itemsPerPage") val itemsPerPage: Int?,
+    @JsonProperty("query") val query: String? = null,
+    @JsonProperty("searchCategoryId") val searchCategoryId: Int? = null,
+    @JsonProperty("searchCategoryName") val searchCategoryName: String? = null,
+    @JsonProperty("item") val item: List<AladinDetailItem> = emptyList()
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AladinDetailItem internal constructor(
+    @JsonProperty("title") val title: String,
+    @JsonProperty("link") val link: String,
+    @JsonProperty("author") val author: String?,
+    @JsonProperty("pubDate") val pubDate: String?,
+    @JsonProperty("description") val description: String?,
+    @JsonProperty("isbn") val isbn: String?,
+    @JsonProperty("isbn13") val isbn13: String,
+    @JsonProperty("itemId") val itemId: Long,
+    @JsonProperty("priceSales") val priceSales: BigDecimal,
+    @JsonProperty("priceStandard") val priceStandard: BigDecimal,
+    @JsonProperty("mallType") val mallType: String,
+    @JsonProperty("stockStatus") val stockStatus: String?,
+    @JsonProperty("mileage") val mileage: Int?,
+    @JsonProperty("cover") val cover: String,
+    @JsonProperty("categoryId") val categoryId: Int,
+    @JsonProperty("categoryName") val categoryName: String,
+    @JsonProperty("publisher") val publisher: String?,
+    @JsonProperty("subInfo") val subInfo: AladinDetailSubInfo
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AladinDetailSubInfo(
+    @JsonProperty("subTitle")
+    val subTitle: String?,
+    @JsonProperty("originalTitle")
+    val originalTitle: String?,
+    @JsonProperty("itemPage")
+    val itemPage: Int?
+)
