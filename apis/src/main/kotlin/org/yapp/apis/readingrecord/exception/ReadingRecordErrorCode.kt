5f2fd6679c0ce768d6e9d3b229e873f592320e8d
DongHoon Lee <dhl1924@naver.com>
2025-08-05 14:54:11 +0900

    [BOOK-179] chore: apis, domain - 오류 코드 메시지를 한국어로 변경

--- apis/src/main/kotlin/org/yapp/apis/readingrecord/exception/ReadingRecordErrorCode.kt
+++ apis/src/main/kotlin/org/yapp/apis/readingrecord/exception/ReadingRecordErrorCode.kt
@@ -8,7 +8,7 @@ enum class ReadingRecordErrorCode(
     private val code: String,
     private val message: String
 ) : BaseErrorCode {
-    READING_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "READING_RECORD_001", "독서 기록을 찾을 수 없습니다.");
+    READING_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, "READING_RECORD_404_01", "독서 기록을 찾을 수 없습니다.");
 
     override fun getHttpStatus(): HttpStatus = status
     override fun getCode(): String = code
--- domain/src/main/kotlin/org/yapp/domain/userbook/exception/UserBookErrorCode.kt
+++ domain/src/main/kotlin/org/yapp/domain/userbook/exception/UserBookErrorCode.kt
@@ -8,7 +8,7 @@ enum class UserBookErrorCode(
     private val code: String,
     private val message: String
 ) : BaseErrorCode {
-    USER_BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_BOOK_001", "유저의 책 정보를 찾을 수 없습니다.");
+    USER_BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_BOOK_404_01", "유저의 책 정보를 찾을 수 없습니다.");
 
     override fun getHttpStatus(): HttpStatus = status
     override fun getCode(): String = code
