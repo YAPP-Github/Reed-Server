84569bf6aa02e2979d06f98efce729c36c566c52
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-08-06 22:33:53 +0900

    [BOOK-227] fix:global-util -  isbn validation 수정(#75)

--- global-utils/src/main/kotlin/org/yapp/globalutils/validator/IsbnValidator.kt
+++ global-utils/src/main/kotlin/org/yapp/globalutils/validator/IsbnValidator.kt
@@ -1,9 +1,12 @@
 package org.yapp.globalutils.validator
 
+import org.yapp.globalutils.util.RegexUtils
+
 object IsbnValidator {
-    private val ISBN_REGEX = Regex("^(.{10}|.{13})$")
+    private val ISBN10_REGEX = Regex(RegexUtils.ISBN10_PATTERN)
+    private val ISBN13_REGEX = Regex(RegexUtils.ISBN13_PATTERN)
 
     fun isValidIsbn(isbn: String): Boolean {
-        return isbn.matches(ISBN_REGEX)
+        return isbn.matches(ISBN10_REGEX) || isbn.matches(ISBN13_REGEX)
     }
 }
