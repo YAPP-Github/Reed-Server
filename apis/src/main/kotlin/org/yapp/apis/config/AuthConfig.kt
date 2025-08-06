2e773c7a14a7a96a13543d5ccbccb72aa8e5ff30
DongHoon Lee <dhl1924@naver.com>
2025-08-07 01:33:33 +0900

    [BOOK-180] feat: apis - 카카오 OAuth 속성 추가 및 AuthConfig 수정

--- apis/src/main/kotlin/org/yapp/apis/config/AuthConfig.kt
+++ apis/src/main/kotlin/org/yapp/apis/config/AuthConfig.kt
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(AppleOauthProperties::class)
+@EnableConfigurationProperties(AppleOauthProperties::class, KakaoOauthProperties::class)
 class AuthConfig
