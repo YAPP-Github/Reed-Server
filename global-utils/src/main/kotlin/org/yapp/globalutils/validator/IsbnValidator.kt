f9df6430ddc5f599ce29df5f9e33590fcc9ef9b6
DongHoon Lee <dhl1924@naver.com>
2025-08-07 19:31:21 +0900

    [BOOK-235] refactor: global-utils - ISBN 유효성 검사 메서드를 ISBN10과 ISBN13으로 분리

--- global-utils/src/main/kotlin/org/yapp/globalutils/validator/IsbnValidator.kt
+++ global-utils/src/main/kotlin/org/yapp/globalutils/validator/IsbnValidator.kt
@@ -6,7 +6,11 @@ object IsbnValidator {
     private val ISBN10_REGEX = Regex(RegexUtils.ISBN10_PATTERN)
     private val ISBN13_REGEX = Regex(RegexUtils.ISBN13_PATTERN)
 
-    fun isValidIsbn(isbn: String): Boolean {
-        return isbn.matches(ISBN10_REGEX) || isbn.matches(ISBN13_REGEX)
+    fun isValidIsbn10(isbn: String): Boolean {
+        return isbn.matches(ISBN10_REGEX)
+    }
+
+    fun isValidIsbn13(isbn: String): Boolean {
+        return isbn.matches(ISBN13_REGEX)
     }
 }
