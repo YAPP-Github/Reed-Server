8fc6fa6111347f45fa86a5037b5397f2b2db4de1
DongHoon Lee <dhl1924@naver.com>
2025-08-08 17:15:08 +0900

    [BOOK-90] feat: apis - Swagger 보안 설정을 비활성화하는 OperationCustomizer 추가

--- apis/src/main/kotlin/org/yapp/apis/config/SwaggerConfig.kt
+++ apis/src/main/kotlin/org/yapp/apis/config/SwaggerConfig.kt
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
+import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.yapp.globalutils.annotation.DisableSwaggerSecurity
 
 @Configuration
 @EnableConfigurationProperties(SwaggerProperties::class)
@@ -49,5 +51,19 @@ class SwaggerConfig(
                             .bearerFormat("JWT")
                     )
             )
+    }
+
+    @Bean
+    fun securityOperationCustomizer(): OperationCustomizer {
+        return OperationCustomizer { operation, handlerMethod ->
+            val disableSwaggerSecurity: DisableSwaggerSecurity? =
+                handlerMethod.getMethodAnnotation(DisableSwaggerSecurity::class.java)
+
+            if (disableSwaggerSecurity != null) {
+                operation.security = listOf()
+            }
+
+            operation
+        }
     }
 }
